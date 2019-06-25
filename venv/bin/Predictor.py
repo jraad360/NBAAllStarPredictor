8f9abb6c800836f28e99b64d7836cab18fc18f09
Jorge Raad <jar121@duke.edu>
2019-06-25 11:30:27 -0400

    Finished data manipulation code. Ready to separate into features.

--- venv/bin/Predictor.py
+++ venv/bin/Predictor.py
@@ -8,31 +8,49 @@ REG_STATS_INDEX = 0
 
 
 # %%
-def get_all_star(id):
-    player_career = playercareerstats.PlayerCareerStats(player_id=id).get_data_frames()
+def get_all_star(id_number):
+    player_career = playercareerstats.PlayerCareerStats(player_id=id_number).get_data_frames()
     all_star_seasons = player_career[ALL_STAR_STATS_INDEX]
     reg_seasons = player_career[REG_STATS_INDEX]
     is_all_star = pd.Series([])
 
     for i in range(len(reg_seasons)):
         year = reg_seasons["SEASON_ID"].iloc[i]
+
         if year in all_star_seasons["SEASON_ID"]:
-            is_all_star = is_all_star.append(pd.Series([True]))
+            is_all_star = is_all_star.append(pd.Series([True]), ignore_index=True)
         else:
-            is_all_star = is_all_star.append(pd.Series([False]))
+            is_all_star = is_all_star.append(pd.Series([False]), ignore_index=True)
 
     return is_all_star
 
+
+def eliminate_duplicate_seasons(seasons):
+    to_remove = []
+
+    for i in range(len(seasons)):
+
+        if (seasons["SEASON_ID"].iloc[i] == seasons["SEASON_ID"].iloc[i-1]) \
+                and (seasons["PLAYER_ID"].iloc[i] == seasons["PLAYER_ID"].iloc[i-1]):
+            to_remove.append(i-1)
+
+    print(to_remove)
+
+    seasons.drop(seasons.index[to_remove], axis=0, inplace=True)
+
+
 #%%
 pd.set_option('display.max_columns', 30)
+#
+# print(players.find_players_by_first_name('lebron'))
+# player = players.find_players_by_first_name('lebron')
+# career_stats = playercareerstats.PlayerCareerStats(player_id=2544).get_data_frames()
+# career_stats = pd.DataFrame(data=career_stats)
+#
+# career_stats = career_stats[0][0]
+# career_stats = pd.DataFrame(data=career_stats)
+career_stats = pd.DataFrame()
 
-print(players.find_players_by_first_name('lebron'))
-player = players.find_players_by_first_name('lebron')
-career_stats = playercareerstats.PlayerCareerStats(player_id=2544).get_data_frames()
-career_stats = pd.DataFrame(data=career_stats)
-
-career_stats = career_stats[0][0]
-career_stats = pd.DataFrame(data=career_stats)
 
 #%%
 active_players = pd.DataFrame(data=players.get_active_players())
@@ -45,9 +63,10 @@ for i in range(0,10):
     stats = playercareerstats.PlayerCareerStats(player_id=player_id).get_data_frames()
     stats = pd.DataFrame(data=stats)
     stats = pd.DataFrame(data=stats[0][0])
-    print(stats)
-    # stats['ALL_STAR'] = get_all_star(player_id)
-    # career_stats = career_stats.append(stats)
-# print(career_stats)
+    all_star_column = get_all_star(player_id)
+    stats["ALL_STAR"] = all_star_column
+    career_stats = career_stats.append(stats, ignore_index=True)
 
-
+#%%
+eliminate_duplicate_seasons(career_stats)
+print(career_stats)
